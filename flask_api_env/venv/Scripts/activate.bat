4dc8625a2b8b56831f68f35b3c2ec6e8ecd687fa
banfstory <denis_chi_yiu@hotmail.com>
2020-10-24 21:44:39 +1100

    fixed

--- flask_api_env/venv/Scripts/activate.bat
+++ flask_api_env/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Denis\Desktop\vue_forum_flask\flask_api_env\venv
+set VIRTUAL_ENV=C:\Users\Denis\Desktop\flask_api_env\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
